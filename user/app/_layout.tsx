202c33f0d313e8a97604a31ace4e78289d001aaf
Puja Roniansyah <puja_roniansyah@teknokrat.ac.id>
2026-05-09 21:41:59 +0700

    ilangin header index

--- user/app/_layout.tsx
+++ user/app/_layout.tsx
@@ -15,9 +15,23 @@ export default function RootLayout() {
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
-      </Stack>
+  <Stack.Screen
+    name="(tabs)"
+    options={{ headerShown: false }}
+  />
+  <Stack.Screen
+    name="index"
+    options={{ headerShown: false }}
+  />
+  <Stack.Screen
+    name="modal"
+    options={{
+      presentation: 'modal',
+      title: 'Modal',
+    }}
+  />
+</Stack>
+
       <StatusBar style="auto" />
     </ThemeProvider>
   );
