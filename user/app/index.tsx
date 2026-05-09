4d2bdff0f195b16d52f7a1fa441322f0a9e60451
Puja Roniansyah <puja_roniansyah@teknokrat.ac.id>
2026-05-09 20:09:46 +0700

    laman utama

---
+++ user/app/index.tsx
@@ -0,0 +1,29 @@
+import {
+  View,
+  Text,
+  TouchableOpacity,
+} from 'react-native';
+
+import { router } from 'expo-router';
+
+export default function HomePage() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <Text>GoTour</Text>
+
+      <TouchableOpacity
+        onPress={() =>
+          router.push('/destination')
+        }
+      >
+        <Text>Lihat Destinasi</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
