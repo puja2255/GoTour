292c3217ff63c839ae1fd0e7ec2f049d0df0e66e
Puja Roniansyah <puja_roniansyah@teknokrat.ac.id>
2026-05-09 21:14:41 +0700

    dummy data

--- user/app/index.tsx
+++ user/app/index.tsx
@@ -8,6 +8,30 @@ import {
   TouchableOpacity,
 } from 'react-native';
 
+const destinations = [
+  {
+    id: 1,
+    name: 'Pantai Mutun',
+    location: 'Lampung',
+    image:
+      'https://picsum.photos/400/300?random=1',
+  },
+  {
+    id: 2,
+    name: 'Pulau Pahawang',
+    location: 'Pesawaran',
+    image:
+      'https://picsum.photos/400/300?random=2',
+  },
+  {
+    id: 3,
+    name: 'Bukit Sakura',
+    location: 'Bandar Lampung',
+    image:
+      'https://picsum.photos/400/300?random=3',
+  },
+];
+
 import { router } from 'expo-router';
 
 export default function HomePage() {
