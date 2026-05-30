8b2380a4dc54176b36a5053516d2f76d6cc33dab
Puja Roniansyah <puja_roniansyah@teknokrat.ac.id>
2026-05-30 19:05:55 +0700

    prefix conroller

--- backend/tour-service/src/category/category.controller.ts
+++ backend/tour-service/src/category/category.controller.ts
@@ -1,3 +1,8 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { CategoryService } from './category.service';
 
+@Controller('category')
+export class CategoryController {
+  constructor(private readonly categoryService: CategoryService) {}
+
+}
