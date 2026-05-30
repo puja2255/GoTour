9795ca39304207c68b4e4eec8197636a482ecd1d
Puja Roniansyah <puja_roniansyah@teknokrat.ac.id>
2026-05-30 19:20:52 +0700

    edit decoator di kategori modul

--- backend/tour-service/src/category/category.module.ts
+++ backend/tour-service/src/category/category.module.ts
@@ -1,3 +1,8 @@
 import { Module } from '@nestjs/common';
 import { CategoryController } from './category.controller';
 import { CategoryService } from './category.service';
+
+@Module({
+  controllers: [CategoryController],
+  providers: [CategoryService],
+})
