5f357284db5d043a9105d04d30d294a8898ec1e8
Puja Roniansyah <puja_roniansyah@teknokrat.ac.id>
2026-05-30 19:16:04 +0700

    pos data kategori

--- backend/tour-service/src/category/category.service.ts
+++ backend/tour-service/src/category/category.service.ts
@@ -4,4 +4,10 @@ import { PrismaService } from '../prisma/prisma.service';
 @Injectable()
 export class CategoryService {
   constructor(private prisma: PrismaService) {}
+
+  create(data: { name: string }) {
+    return this.prisma.category.create({
+      data,
+    });
+  }
 }
