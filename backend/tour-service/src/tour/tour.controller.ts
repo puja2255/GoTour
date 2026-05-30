a9c7e67e903ea159b33c5242ddad8084efe62da7
Puja Roniansyah <puja_roniansyah@teknokrat.ac.id>
2026-05-30 14:39:22 +0700

    import dto di tur control

--- backend/tour-service/src/tour/tour.controller.ts
+++ backend/tour-service/src/tour/tour.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Get, Post, Body } from '@nestjs/common';
 import { TourService } from './tour.service';
+import { CreateTourDto } from './dto/create-tour.dto';
 
 @Controller('tour')
 export class TourController {}
