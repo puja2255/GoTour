4e5cfe8c7882987dd31e00323dc8318020e1c88a
Puja Roniansyah <puja_roniansyah@teknokrat.ac.id>
2026-05-30 19:23:26 +0700

    edit app modul

--- backend/tour-service/src/app.module.ts
+++ backend/tour-service/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TourModule } from './tour/tour.module';
+import { CategoryModule } from './category/category.module';
 
 @Module({
-  imports: [TourModule],
+  imports: [TourModule, CategoryModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
