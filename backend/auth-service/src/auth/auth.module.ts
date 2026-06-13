7c129dff43e35a83de7eeed5a8b28bb56d7baeb8
Puja Roniansyah <puja_roniansyah@teknokrat.ac.id>
2026-06-13 22:17:25 +0700

    impor jwt

--- backend/auth-service/src/auth/auth.module.ts
+++ backend/auth-service/src/auth/auth.module.ts
@@ -3,6 +3,7 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { PrismaService } from '../prisma/prisma.service';
 import { JwtModule } from '@nestjs/jwt';
+import { JwtGuard } from './guards/jwt.guard';
 
 @Module({
   imports: [
