3cff358526a99e20ca29ff662089338824dd1baa
Puja Roniansyah <puja_roniansyah@teknokrat.ac.id>
2026-06-13 22:13:23 +0700

    get request

--- backend/auth-service/src/auth/guards/jwt.guard.ts
+++ backend/auth-service/src/auth/guards/jwt.guard.ts
@@ -13,5 +13,8 @@ export class JwtGuard implements CanActivate {
   ) {}
 
   canActivate(
-    
+    context: ExecutionContext,
+  ): boolean {
+    const request =
+      context.switchToHttp().getRequest();
   )
