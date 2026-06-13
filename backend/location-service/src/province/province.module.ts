aea9992bcc6d67376e0b3a04b96fe66ca27355e6
Puja Roniansyah <puja_roniansyah@teknokrat.ac.id>
2026-06-13 22:56:17 +0700

    moduul

--- backend/location-service/src/province/province.module.ts
+++ backend/location-service/src/province/province.module.ts
@@ -3,12 +3,8 @@ import { ProvinceController } from './province.controller';
 import { ProvinceService } from './province.service';
 import { PrismaService } from '../prisma/prisma.service';
 
-
 @Module({
   controllers: [ProvinceController],
-  providers: [
-    ProvinceService,
-    PrismaService,
-  ],
+  providers: [ProvinceService, PrismaService],
 })
 export class ProvinceModule {}
