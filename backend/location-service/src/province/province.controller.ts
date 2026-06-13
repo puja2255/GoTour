4374ea7b149440d4e212291ed05bdd54f1e6e9d5
Puja Roniansyah <puja_roniansyah@teknokrat.ac.id>
2026-06-13 22:53:31 +0700

    rapihin

--- backend/location-service/src/province/province.controller.ts
+++ backend/location-service/src/province/province.controller.ts
@@ -1,29 +1,19 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-} from '@nestjs/common';
+import { Controller, Get, Post, Body } from '@nestjs/common';
 
 import { ProvinceService } from './province.service';
 @Controller('province')
 export class ProvinceController {}
 @Controller('province')
 export class ProvinceController {
-  constructor(
-    private readonly service: ProvinceService,
-  ) {}
+  constructor(private readonly service: ProvinceService) {}
 
   @Post()
-  create(
-    @Body() body: { name: string },
-  ) {
+  create(@Body() body: { name: string }) {
     return this.service.create(body);
   }
 
-   @Get()
+  @Get()
   findAll() {
     return this.service.findAll();
   }
 }
-
