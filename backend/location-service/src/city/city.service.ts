806cb847ee79083e9d2ff5f8508a67ece86d64e3
Puja Roniansyah <puja_roniansyah@teknokrat.ac.id>
2026-06-13 23:10:50 +0700

    kelas siti servis

--- backend/location-service/src/city/city.service.ts
+++ backend/location-service/src/city/city.service.ts
@@ -1,4 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 @Injectable()
-export class CityService {}
+export class CityService {
+  constructor(
+    private readonly prisma: PrismaService,
+  ) {}
