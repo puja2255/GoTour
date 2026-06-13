ddc0be5a03bc44a5055531e12601825306a27ef3
Puja Roniansyah <puja_roniansyah@teknokrat.ac.id>
2026-06-13 23:23:27 +0700

    perbaiki main lokasi

--- backend/location-service/src/main.ts
+++ backend/location-service/src/main.ts
@@ -3,6 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3021);
+
+  app.enableCors();
+
+  await app.listen(3002);
 }
+
 bootstrap();
