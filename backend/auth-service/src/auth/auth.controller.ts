6a582ed5f3c7ff82a4d7ae7b7932ee8cdcd0a1cd
Puja Roniansyah <puja_roniansyah@teknokrat.ac.id>
2026-06-13 20:59:50 +0700

    import login di aut contro

--- backend/auth-service/src/auth/auth.controller.ts
+++ backend/auth-service/src/auth/auth.controller.ts
@@ -6,5 +6,6 @@ import {
 
 import { AuthService } from './auth.service';
 import { RegisterDto } from './dto/register.dto';
+import { LoginDto } from './dto/login.dto';
 @Controller('auth')
 export class AuthController {}
