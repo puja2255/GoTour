9d055d9dccea12c9dc1dfd85d00b4abe542df7d1
Puja Roniansyah <puja_roniansyah@teknokrat.ac.id>
2026-06-13 20:46:46 +0700

    impor bcrypt

--- backend/auth-service/src/auth/auth.service.ts
+++ backend/auth-service/src/auth/auth.service.ts
@@ -3,6 +3,7 @@ import { PrismaService } from '../prisma/prisma.service';
 import { RegisterDto } from './dto/register.dto';
 import { LoginDto } from './dto/login.dto';
 import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {}
