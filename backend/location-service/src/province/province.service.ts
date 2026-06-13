604e0df1a1be52bb6bbdc6d25316caf3f358853d
Puja Roniansyah <puja_roniansyah@teknokrat.ac.id>
2026-06-13 23:09:21 +0700

    rapihin

--- backend/location-service/src/province/province.service.ts
+++ backend/location-service/src/province/province.service.ts
@@ -2,17 +2,15 @@ import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 @Injectable()
 export class ProvinceService {
-  constructor(
-    private prisma: PrismaService,
-  ) {}
+  constructor(private prisma: PrismaService) {}
 
-    create(data: { name: string }) {
+  create(data: { name: string }) {
     return this.prisma.province.create({
       data,
     });
   }
 
-    findAll() {
+  findAll() {
     return this.prisma.province.findMany();
   }
 }
