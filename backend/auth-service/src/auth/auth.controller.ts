cf7158971a70dd5983703ea51ea9bfffcee5594a
Puja Roniansyah <puja_roniansyah@teknokrat.ac.id>
2026-06-13 20:59:27 +0700

    import register di aut contro

--- backend/auth-service/src/auth/auth.controller.ts
+++ backend/auth-service/src/auth/auth.controller.ts
@@ -5,5 +5,6 @@ import {
 } from '@nestjs/common';
 
 import { AuthService } from './auth.service';
+import { RegisterDto } from './dto/register.dto';
 @Controller('auth')
 export class AuthController {}
