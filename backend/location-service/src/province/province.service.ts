0f2baf6f972f7934b777c66b85de2236071b3274
Puja Roniansyah <puja_roniansyah@teknokrat.ac.id>
2026-06-13 22:49:36 +0700

    clas provinsi

--- backend/location-service/src/province/province.service.ts
+++ backend/location-service/src/province/province.service.ts
@@ -1,4 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 @Injectable()
-export class ProvinceService {}
+export class ProvinceService {
+  constructor(
+    private prisma: PrismaService,
+  ) {}
