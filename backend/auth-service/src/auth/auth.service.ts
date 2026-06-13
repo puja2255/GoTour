5144a851965bcc7a730107dbbcf1f09133731511
Puja Roniansyah <puja_roniansyah@teknokrat.ac.id>
2026-06-13 20:45:41 +0700

    impor register

--- backend/auth-service/src/auth/auth.service.ts
+++ backend/auth-service/src/auth/auth.service.ts
@@ -1,5 +1,5 @@
 import { Injectable, BadRequestException } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
-
+import { RegisterDto } from './dto/register.dto';
 @Injectable()
 export class AuthService {}
