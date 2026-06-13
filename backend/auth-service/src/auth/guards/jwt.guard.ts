879cae6cee4148073f2ee0704e08f8833fcaba3f
Puja Roniansyah <puja_roniansyah@teknokrat.ac.id>
2026-06-13 22:12:16 +0700

    export jwt

--- backend/auth-service/src/auth/guards/jwt.guard.ts
+++ backend/auth-service/src/auth/guards/jwt.guard.ts
@@ -5,3 +5,9 @@ import {
   UnauthorizedException,
 } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
+
+@Injectable()
+export class JwtGuard implements CanActivate {
+  constructor(
+    private jwtService: JwtService,
+  ) {}
