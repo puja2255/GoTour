12403972b18dd958920a937909bbf86c6389c44c
Puja Roniansyah <puja_roniansyah@teknokrat.ac.id>
2026-06-13 23:22:42 +0700

    ganti app modul

--- backend/location-service/src/app.module.ts
+++ backend/location-service/src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 
 import { ProvinceModule } from './province/province.module';
+import { CityModule } from './city/city.module';
+import { DistrictModule } from './district/district.module';
 
 @Module({
-  imports: [ProvinceModule],
+  imports: [ProvinceModule, CityModule, DistrictModule],
 })
 export class AppModule {}
