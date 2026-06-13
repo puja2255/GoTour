3a4500c10e74c0ab1a8f04e9568e3f3c456e60a1
Puja Roniansyah <puja_roniansyah@teknokrat.ac.id>
2026-06-13 22:19:19 +0700

    impor jwt di controler

--- backend/auth-service/src/auth/auth.controller.ts
+++ backend/auth-service/src/auth/auth.controller.ts
@@ -3,6 +3,7 @@ import { Controller, Post, Body } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { RegisterDto } from './dto/register.dto';
 import { LoginDto } from './dto/login.dto';
+import { JwtGuard } from './guards/jwt.guard';
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
