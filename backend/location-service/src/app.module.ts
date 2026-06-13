fe24fb0f64651f6cc8df33ac304798e3b1d1b35f
Puja Roniansyah <puja_roniansyah@teknokrat.ac.id>
2026-06-13 22:57:23 +0700

    lidahku biru

--- backend/location-service/src/app.module.ts
+++ backend/location-service/src/app.module.ts
@@ -1,13 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+
 import { ProvinceModule } from './province/province.module';
-import { CityModule } from './city/city.module';
-import { DistrictModule } from './district/district.module';
 
 @Module({
-  imports: [ProvinceModule, CityModule, DistrictModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [ProvinceModule],
 })
 export class AppModule {}
