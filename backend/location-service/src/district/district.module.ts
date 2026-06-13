dd458a5ab650be03135e058d82f645bd7e2855ff
Puja Roniansyah <puja_roniansyah@teknokrat.ac.id>
2026-06-13 23:21:57 +0700

    rapihin modul

--- backend/location-service/src/district/district.module.ts
+++ backend/location-service/src/district/district.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { DistrictController } from './district.controller';
 import { DistrictService } from './district.service';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Module({
   controllers: [DistrictController],
-  providers: [DistrictService]
+  providers: [DistrictService, PrismaService],
 })
 export class DistrictModule {}
