537e4b655023460813c789fa0e8edeb126c231ec
Puja Roniansyah <puja_roniansyah@teknokrat.ac.id>
2026-06-13 21:07:57 +0700

    impor prismasercv di autmodul

--- backend/auth-service/src/auth/auth.module.ts
+++ backend/auth-service/src/auth/auth.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { PrismaService } from '../prisma/prisma.service';
+
 
 @Module({
   providers: [AuthService],
