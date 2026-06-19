865eaad47a3ad375655d6649e1543e7dcf96388a
bijiapel <m_reza_pratama@teknokrat.ac.id>
2026-06-19 20:54:54 +0700

    Menambahkan import React pada index.tsx

---
+++ backend/auth-service/src/user/dto/update-user.dto.ts
@@ -0,0 +1,20 @@
+import { IsOptional, IsString, MinLength, IsEmail } from 'class-validator';
+
+export class UpdateUserDto {
+  @IsOptional()
+  @IsEmail()
+  email?: string;
+
+  @IsOptional()
+  @IsString()
+  @MinLength(6)
+  password?: string;
+
+  @IsOptional()
+  @IsString()
+  name?: string;
+
+  @IsOptional()
+  @IsString()
+  role?: string;
+}
